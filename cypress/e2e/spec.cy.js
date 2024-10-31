70d72b38d160de1831c556ab03acf1f60e853222
jagadishmoharana <jagadishmoharana02@gmail.com>
2024-10-31 12:05:19 +0530

    first commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,25 @@
+describe('Login page Test', () => {
+  beforeEach(() => {
+    cy.visit('https://the-internet.herokuapp.com/login')
+  });
+  afterEach(() => {
+    cy.clearCookies();
+  });
+  // Check if the login button is visible.
+  it('login button should be visible', () => {
+    cy.get('[type="submit"]').should('be.visible');
+  })
+
+  //verifies if the page contains a field with the placeholder text "Enter your email".
+  it('Verifies if there is a field with the placeholder text "Enter your email"',()=>{
+    cy.document().then((doc) => {
+      const emailInput = doc.querySelector('input[placeholder="Enter your email"]');
+      if (emailInput) {
+        cy.wrap(emailInput).should('exist');
+      } else {
+        cy.log('Email input field with placeholder "Enter your email" is missing');
+        // Optional: Additional actions if the field is missing
+      }
+    })
+  })
+})
